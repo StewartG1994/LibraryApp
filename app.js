e9f4038ed0c9afb95d3179321108fae92227aa0f
Stewart Gliniecki <stewgliniecki@gmail.com>
2023-09-14 19:46:36 +0100

    Build card function from data

---
+++ app.js
@@ -0,0 +1,50 @@
+const submit = document.getElementById('submit')
+const bookArea = document.querySelector('.bookCards')
+let title = document.getElementById('title')
+let author = document.getElementById('author')
+let pages = document.getElementById('pages')
+
+const myLibrary = []
+
+function Book(title, pages, author){
+    this.title = title;
+    this.pages = pages;
+    this.author = author
+
+}
+
+function addBookToLibrary (title, pages, author){
+    let newBook = new Book(title,pages,author)
+    myLibrary.push(newBook)
+}
+
+submit.addEventListener('click' , () =>{
+event.preventDefault()
+addBookToLibrary(title.value, pages.value, author.value)
+runBuildACard()
+console.log(myLibrary)
+})
+
+function runBuildACard(){
+    for (let i = 0; i < myLibrary.length; i++){
+        item = myLibrary[i]
+        buildACard(item)
+    }
+}
+
+function buildACard(item){
+let card = document.createElement('div')
+let titleElement = document.createElement('p')
+let pageElement = document.createElement('p')
+let authorElement = document.createElement('p')
+
+titleElement.textContent = item.title;
+pageElement.textContent = item.pages;
+authorElement.textContent = item.author
+
+card.appendChild(titleElement)
+card.appendChild(pageElement)
+card.appendChild(authorElement)
+
+bookArea.appendChild(card)
+}
