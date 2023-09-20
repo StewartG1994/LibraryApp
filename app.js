661e7f0f3c71acd6eceed58a33bd1eb9085b7b10
Stewart Gliniecki <stewgliniecki@gmail.com>
2023-09-20 16:45:22 +0100

    Delete function

--- app.js
+++ app.js
@@ -42,10 +42,15 @@ function runBuildACard(){
 function buildACard(item){
 let card = document.createElement('div')
 card.className = 'card'
+let index = myLibrary.indexOf(item)
+card.setAttribute('cardNumber', index)
 let titleElement = document.createElement('p')
 let pageElement = document.createElement('p')
 let authorElement = document.createElement('p')
+const deleteButton = document.createElement('button')
+deleteButton.className = 'deleteBtn'
 
+deleteButton.textContent = 'Delete Book'
 titleElement.textContent = item.title;
 pageElement.textContent = item.pages;
 authorElement.textContent = item.author
@@ -53,6 +58,21 @@ authorElement.textContent = item.author
 card.appendChild(titleElement)
 card.appendChild(pageElement)
 card.appendChild(authorElement)
+card.appendChild(deleteButton)
 
 bookArea.appendChild(card)
+
+cardDelete(deleteButton)
 }
+
+function cardDelete (deleteButton) {
+    deleteButton.addEventListener('click',  event => {
+        let deleteCard = event.target.parentElement;
+        let indexInfo = deleteCard.getAttribute('cardNumber');    
+        bookArea.removeChild(deleteCard)
+        myLibrary.splice(Number(indexInfo), 1);
+    })
+   
+}
+
+
